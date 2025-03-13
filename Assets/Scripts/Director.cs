ca825d4ff54b9feece3d3a1552cb52c65ca5cb41
miura <a.miura0000@gmail.com>
2025-03-13 16:36:01 +0900

    ファイル追加

---
+++ Assets/Scripts/Director.cs
@@ -0,0 +1,15 @@
+/*! @file	Director.cs
+	@brief	制御用クラス
+
+	@author miura
+ */
+using UnityEngine;
+
+namespace SentisSD
+{
+	public class Director : MonoBehaviour
+	{
+
+
+	}
+}	// namespace SentisSD
